3d0dbf68d9a648a4bc5640ee4c6b9ed148860d0e
Johnathan Brennan <jbrennan4306@gmail.com>
2021-10-15 10:49:32 -0500

    renamed files

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
+import Header from './components/header.jsx';
+import UserPage from './components/userPage.jsx';
+import ResearchPage from './components/researchPage.jsx';
+import AnimalPage from './components/animalPage.jsx';
 import "./styles.css";
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 
 const App = () => {
   return (
-    <h1 className="app">
-      Hello World!
-    </h1>
+    <Router>
+      <h1 className="app">
+        <Header />
+        <Route path='/user'/>
+        <Route path='/research'/>
+        <Route path='/dogProfile' />
+      </h1>
+    </Router>
   )
 };
 
