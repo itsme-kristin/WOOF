8d3c8f96ed1c2c62c39a7d2a4c9128177141b34e
robwilliams-it <robwilliams.it@gmail.com>
2021-10-15 11:22:08 -0500

    updated missing mui packages, updated card to be dog card

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import "./styles.css";
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import DogCard from './components/card/card.jsx';
 
 
 const App = () => {
   return (
-    <h1 className="app">
-      Hello World!
-    </h1>
+    // <h1 className="app">
+    //   Hello World!
+    // </h1>
+    <DogCard />
   )
 };
 
