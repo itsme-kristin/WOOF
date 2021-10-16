5e58750c42822a85a744a29c35fb9e37538eb2c5
Kristin Gadgil <kristingadgil@gmail.com>
2021-10-16 10:52:56 -0500

    Added breed schema

--- db/index.js
+++ db/index.js
@@ -1,5 +1,6 @@
 const mongoose = require ('mongoose');
 const config = require ('./config.js');
+const axios = require ('axios');
 
 mongoose.connect(config.mongoURI)
   .then(() => {
@@ -16,11 +17,37 @@ mongoose.connect(config.mongoURI)
     savedDogs: Object,
   })
 
+  const breedSchema = new mongoose.Schema({
+    bred_for: String,
+    breed_group: String,
+    country_code: {
+      type: String,
+      default: ''
+    },
+    height: {
+      imperial: String,
+      metric: String
+    },
+    id: Number,
+    image: {
+      height: Number,
+      id: String,
+      url: String,
+      width: Number,
+    },
+    life_span: String,
+    name: String,
+    origin: String,
+    reference_image_id: String,
+    temperament: String,
+    weight: {
+      imperial: String,
+      metric: String
+    }
+
+  })
+
   const User = mongoose.model('User', userSchema);
 
-  User.create({
-    name: 'Cinderella',
-    email: 'ilostmyshoe.com',
-    password: 'loveshoes',
-    savedDogs: {'Fido': 'Fido'}
-  })
+  const Breed = mongoose.model('Breed', breedSchema);
+
