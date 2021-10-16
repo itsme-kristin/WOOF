923f47de1606a435e6602e0708a38c739bbcc94d
Donald Vida <donald.vida@gmail.com>
2021-10-16 13:57:04 -0500

    Refactored header with initial styling

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -1,12 +1,40 @@
 import React from 'react';
 
+import {
+  Box,
+  Typography,
+  Grid,
+} from '@mui/material';
+import PetsIcon from '@mui/icons-material/Pets';
+
+const logo = {
+  fontSize: '55px',
+  color: 'white',
+  transform: 'rotate(-30deg)',
+  paddingLeft: '2px',
+}
+
 const Header = () => {
   return (
     <nav>
-      <h1>Logo</h1>
-      <ul>
-        <li>Research Breeds</li>
-      </ul>
+        <Grid container spacing={1} alignItems="center" className="header">
+          <Grid item xs={1} class="logo">
+            <PetsIcon sx={logo}/>
+          </Grid>
+          <Grid item xs={6} alignSelf="flex-end">
+            <Typography class="title">
+              Woof
+            </Typography>
+          </Grid>
+          <Grid item xs={4} className="nav">
+            <ul>
+              <li>Services</li>
+              <li>Breeds</li>
+              {/* Conditional Rendering */}
+              <li>Login</li>
+            </ul>
+          </Grid>
+        </Grid>
     </nav>
   )
 }
