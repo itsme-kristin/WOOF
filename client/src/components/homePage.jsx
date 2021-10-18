fc1e7953e5be12c503b0a9a52efd520c3cdc1c48
Donald Vida <donald.vida@gmail.com>
2021-10-18 12:23:29 -0500

    Added the image overlay for the research breed teaser

--- client/src/components/homePage.jsx
+++ client/src/components/homePage.jsx
@@ -16,13 +16,18 @@ const searchBar = () => {
     marginTop: '40px',
   }
 
+  const dogButton = {
+    height: "56px",
+    width: "164px",
+  }
+
   return (
     <Grid container justifyContent="center" spacing={1} sx={homeSearch}>
       <Grid item xs={3}>
         <TextField label="Enter your Zip Code" variant="outlined" fullWidth/>
       </Grid>
       <Grid item xs={2}>
-        <Button variant="contained" size="large" endIcon={<SearchIcon />} sx={{ height: "56px"}}>
+        <Button variant="contained" size="large" endIcon={<SearchIcon />} sx={dogButton}>
           Find a Dog
         </Button>
       </Grid>
@@ -47,6 +52,30 @@ const listTop5 = () => {
   );
 }
 
+const researchBreeds = () => {
+  const image='https://i0.wp.com/images.ctfassets.net/440y9b545yd9/4xeV5wnpElbhV0aG2wTiQy/c8d921df3e81977dcdc5a03706502bc4/dog_pack_top10_850__1_.jpg'
+
+
+  const overlay = {
+    width: "95%",
+    height: "320px",
+    borderRadius: '10px',
+    backgroundImage: `url(${image}), linear-gradient(rgba(196,196,196,0.7),rgba(196,196,196,196.7))`,
+    backgroundBlendMode: 'overlay',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+    marginBottom: '20px',
+  }
+
+  return (
+    <Grid container justifyContent="center">
+      <Grid item sx={overlay}>
+        Research Breeds
+      </Grid>
+    </Grid>
+  )
+}
+
 const HomePage = () => {
   return (
     < >
@@ -63,7 +92,7 @@ const HomePage = () => {
             {listTop5()}
         </Grid>
         <Grid item xs={12}>
-          Overlay
+          {researchBreeds()}
         </Grid>
       </Grid>
     </>
