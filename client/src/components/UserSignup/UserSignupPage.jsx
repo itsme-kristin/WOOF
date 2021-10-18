af69a679fcf02e13e5177f3e66ade0276a6f1e46
Johnathan Brennan <jbrennan4306@gmail.com>
2021-10-18 11:01:17 -0500

    added fields for user sign up

--- client/src/components/UserSignup/UserSignupPage.jsx
+++ client/src/components/UserSignup/UserSignupPage.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
 
-import Typography from '@mui/material/Typography';
+import { Typography, TextField, Grid } from '@mui/material/';
+
+
+const tempStyle = {
+  border: '3px solid black',
+}
 
 const UserSignup = () => {
   return (
     <div>
-      <Typography variant='h3' >Sign Up Page</Typography>
+      <br />
+      <Grid container spacing={3} alignItems='center' justifyContent='center' direction='column' style={tempStyle}>
+        <Grid item>
+          <Typography variant='h3'>Sign Up Page</Typography>
+        </Grid>
+        <Grid item>
+          <TextField id='usernameField' label='Username' variant='outlined' required/>
+        </Grid>
+        <Grid item>
+          <TextField id='passwordField' label='Password' variant='outlined' required/>
+        </Grid>
+        <Grid item>
+          <TextField id='emailField' label='Email' variant='outlined' required/>
+        </Grid>
+        <Grid item>
+          <Typography variant='subtitle1'>Address Field</Typography>
+          <TextField id='addressField' label='Address' variant='outlined' required/>
+        </Grid>
+      </Grid>
     </div>
   )
 }
