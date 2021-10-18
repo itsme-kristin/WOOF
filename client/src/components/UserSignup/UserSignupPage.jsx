4733093a678dc0a90f9b5338119d111abaaec39e
Johnathan Brennan <jbrennan4306@gmail.com>
2021-10-18 11:35:18 -0500

    imported config files and env variables for firebase

--- client/src/components/UserSignup/UserSignupPage.jsx
+++ client/src/components/UserSignup/UserSignupPage.jsx
@@ -1,34 +1,43 @@
 import React from 'react';
 
-import { Typography, TextField, Grid } from '@mui/material/';
+import { Typography,
+  TextField,
+  Grid,
+  Button,
+  Card } from '@mui/material/';
 
-
-const tempStyle = {
-  border: '3px solid black',
-}
 
 const UserSignup = () => {
   return (
     <div>
       <br />
-      <Grid container spacing={3} alignItems='center' justifyContent='center' direction='column' style={tempStyle}>
-        <Grid item>
-          <Typography variant='h3'>Sign Up Page</Typography>
-        </Grid>
-        <Grid item>
-          <TextField id='usernameField' label='Username' variant='outlined' required/>
-        </Grid>
-        <Grid item>
-          <TextField id='passwordField' label='Password' variant='outlined' required/>
-        </Grid>
-        <Grid item>
-          <TextField id='emailField' label='Email' variant='outlined' required/>
-        </Grid>
-        <Grid item>
-          <Typography variant='subtitle1'>Address Field</Typography>
-          <TextField id='addressField' label='Address' variant='outlined' required/>
-        </Grid>
+      <Grid container spacing={3} alignItems='center' justifyContent='center' direction='column'>
+        <Card>
+          <Grid item>
+            <Typography variant='h3'>Sign Up</Typography>
+          </Grid>
+          <form id='signupForm'>
+            <Grid item>
+              <TextField id='usernameField' label='Username' variant='outlined' required/>
+            </Grid>
+            <Grid item>
+              <TextField id='passwordField' label='Password' variant='outlined' required/>
+            </Grid>
+            <Grid item>
+              <TextField id='emailField' label='Email' variant='outlined' required/>
+            </Grid>
+            <Grid item>
+              <Typography variant='subtitle1'>Address Field</Typography>
+              <TextField id='addressField' label='Address' variant='outlined' required/>
+            </Grid>
+            <Button onClick={handleClick}>Submit</Button>
+          </form>
+        </Card>
       </Grid>
+      <Typography variant='body1'>Already have an account? Login
+        {/* <Link>
+        </Link> */}
+      </Typography>
     </div>
   )
 }
