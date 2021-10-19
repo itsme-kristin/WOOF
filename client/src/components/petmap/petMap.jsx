3217a9b878367eb42217e0cd522baa082fce0277
aliciav-texas <aliciav.texas@gmail.com>
2021-10-19 12:33:09 -0500

    "initial rendering of map hard coded to a location"

--- client/src/components/petmap/petMap.jsx
+++ client/src/components/petmap/petMap.jsx
@@ -1,7 +1,34 @@
-import React from 'react';
+import React from "react";
+import {
+  GoogleMap,
+  withScriptjs,
+  withGoogleMap,
+  Marker,
+} from "react-google-maps";
+import { googleAPI } from "../../../../config.js";
+
+const googleMap = () => {
+  return (
+    <GoogleMap
+      defaultZoom={10}
+      defaultCenter={{ lat: 30.542747, lng: -97.550011 }}
+    >
+      <Marker position={{ lat: 30.542747, lng: -97.550011 }} />
+    </GoogleMap>
+  );
+};
+
+const WrappedMap = withScriptjs(withGoogleMap(googleMap));
 
 const PetMap = () => {
-  return <div />
-}
+  return (
+    <WrappedMap
+      googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${googleAPI}`}
+      loadingElement={<div style={{ height: `100%` }} />}
+      containerElement={<div style={{ height: `197px` }} />}
+      mapElement={<div style={{ height: `100%` }} />}
+    />
+  );
+};
 
 export default PetMap;
