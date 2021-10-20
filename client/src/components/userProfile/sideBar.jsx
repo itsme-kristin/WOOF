8219305d2765cd83879c03e55ac56411c6420736
Donald Vida <donald.vida@gmail.com>
2021-10-20 15:10:58 -0500

    Refactored account form with styling

--- client/src/components/userProfile/sideBar.jsx
+++ client/src/components/userProfile/sideBar.jsx
@@ -1,19 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import UserMenu from './userMenu.jsx';
-
-import Typography from '@mui/material/Typography'
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
 import { useAuth } from '../../contexts/AuthContext.jsx';
+
+import {
+  Box,
+  Button,
+  Typography,
+  Grid,
+ } from '@mui/material';
+
+const style = {
+  form: {
+    title: {
+      fontWeight: 600,
+    },
+    label: 'caption',
+    list: {
+      listStyleType: 'none',
+      paddingLeft: '10px',
+    },
+    buttons: {
+      textAlign: 'right',
+    },
+  },
+};
 
 
 const SideBar = () => {
+  const [editMode, setEditMode] = useState(false);
 
   const { userData, currentUser } = useAuth();
   const [userDataState, setUserDataState] = userData;
-  {console.log(userDataState)}
-  {console.log(currentUser)}
+  // {console.log(userDataState)}
+  // {console.log(currentUser)}
+
+  const toggleEdit = () => {
+    setEditMode(!editMode);
+  };
 
   return (
     <Grid container spacing={2} direction='column'>
@@ -23,11 +47,50 @@ const SideBar = () => {
         </Typography>
       </Grid>
       <Grid item id='userInfo' sm={2}>
-        <Typography>Name: {userDataState.name}</Typography>
-        <Typography>Email: {userDataState.email}</Typography>
-        <Typography>address here</Typography>
-        {/* <Typography>{currentUser.email}</Typography> */}
-        <UserMenu />
+        <Box id="account-form" component="form">
+          <Typography sx={style.form.title}>
+            Location:
+          </Typography>
+          <Typography sx={style.form.list} component='ul' gutterBottom>
+            <li>
+              <Typography variant={style.form.label} component="div">Name:</Typography>
+              {userDataState.name}
+            </li>
+            <li>
+              <Typography variant={style.form.label} component="div">Street:</Typography>
+              {userDataState.street_address}
+            </li>
+            <li>
+              <Typography variant={style.form.label} component="div">City:</Typography>
+              {userDataState.city}
+            </li>
+            <li>
+              <Typography variant={style.form.label} component="div">State:</Typography>
+              {userDataState.state}
+            </li>
+            <li>
+              <Typography variant={style.form.label} component="div">PostalCode:</Typography>
+              {userDataState.zip}
+            </li>
+          </Typography>
+          <Typography sx={style.form.title}>
+            Login Information:
+          </Typography>
+          <Typography sx={style.form.list} component='ul' gutterBottom>
+            <li>
+              <Typography variant={style.form.label} component="div">E-Mail:</Typography>
+              {userDataState.email}
+            </li>
+            <li>
+              <Typography variant={style.form.label} component="div">Password:</Typography>
+              {userDataState.password}
+            </li>
+          </Typography>
+          <Box id="account-form-buttons" component="div" sx={style.form.buttons}>
+            {editMode ? (<Button variant="contained" onClick={toggleEdit}>Save</Button>)
+                      : (<Button variant="contained" onClick={toggleEdit}>Edit</Button>)}
+          </Box>
+        </Box>
       </Grid>
     </Grid>
   )
