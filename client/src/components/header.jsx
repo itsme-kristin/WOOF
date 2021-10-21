e72bf770f91014c7b509f56f8bf48e90b2270a58
Johnathan Brennan <jbrennan4306@gmail.com>
2021-10-20 20:15:20 -0500

    hotfix, but new dogCard error

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -117,7 +117,16 @@ const Header = () => {
                       startIcon={<LogoutIcon sx={icon} />}
                       onClick={() => {
                         signout()
-                        setUserDataState()
+                        setUserDataState({
+                          name: '',
+                          email: '',
+                          password: '',
+                          street_address: '',
+                          city: '',
+                          state: '',
+                          zip: '',
+                          savedBreeds: [],
+                          savedDogs: [],})
                       }
                       }
                     >
